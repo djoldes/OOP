deb98097a74bc9cb939d88b586b30f7389a1e1d1
djoldes <davejoldes@gmail.com>
2023-03-07 08:26:23 +0200

    Add project files.

---
+++ 7_03_L/Program.cs
@@ -0,0 +1,28 @@
+﻿Stack<int> st = new Stack<int>();
+for(int i = 0; i < 10; i++)
+{
+    st.Push(i); 
+}
+while(st.Count > 0)
+{
+    Console.WriteLine(st.Pop());
+}
+try
+{
+    st.Pop();
+}
+catch(InvalidOperationException e)
+{
+    Console.WriteLine(e.Message);
+}
+int result;
+bool ok = st.TryPop(out result);
+if (ok)
+{
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Pop failed. Stack empty");
+}
+st.
